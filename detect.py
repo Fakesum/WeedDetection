7e9d08a4aa6441c294afeba90e6a54721b82e36d
Ansh Mathur <worldanvilbild@gmail.com>
2024-11-30 18:03:09 +0530

    Init

---
+++ detect.py
@@ -0,0 +1,13 @@
+import os, sys, pathlib
+from PIL import Image
+import matplotlib.pyplot as plt
+import shutil
+
+shutil.rmtree("runs")
+
+file_name = sys.argv[1]
+os.system(f"yolo predict model='best.onnx' source={file_name}")
+
+plt.imshow(Image.open(f"./runs/detect/predict/{".".join(os.path.basename(file_name).split(".")[:-1])}.jpg"))
+plt.axis('off') 
+plt.show()
